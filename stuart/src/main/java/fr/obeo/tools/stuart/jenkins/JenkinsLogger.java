a99a280ad367a809e23eda5b8bb59de32f5bc8ee
cbrun <cedric.brun@obeo.fr>
2016-01-13 18:04:14 +0100

    Trying another report format (more dense)

--- stuart/src/main/java/fr/obeo/tools/stuart/jenkins/JenkinsLogger.java
+++ stuart/src/main/java/fr/obeo/tools/stuart/jenkins/JenkinsLogger.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import org.apache.commons.collections.MultiMap;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Predicate;
@@ -82,16 +81,12 @@ public class JenkinsLogger {
 							boolean manualTrigger = false;
 							int totalCount = 0;
 							boolean hasRecentRegressions = false;
-							int failCount = 0;
-							int skipCount = 0;
 							String testReportURLName = null;
 
 							String testsResults = "";
 
 							for (BuildAction action : lastBuild.getActions()) {
 								totalCount += action.getTotalCount();
-								failCount += action.getFailCount();
-								skipCount += action.getSkipCount();
 								if (totalCount > 0) {
 									testReportURLName = action.getUrlName();
 									String rootReportURL = j.getLastBuild().getUrl() + testReportURLName;
@@ -104,55 +99,8 @@ public class JenkinsLogger {
 
 									Map<String, TestReport> reports = allNonEmptyReports(rootReport, rootReportURL);
 									StringBuffer reportString = new StringBuffer();
-									reportString.append(
-											"| Configuration        | Duration  | All  | Failed | Skipped | Age |\n");
-									reportString.append(
-											"| -------------------- |:---------:| ----:|-------:|--------:|-----|\n");
-									for (Map.Entry<String, TestReport> r : reports.entrySet()) {
-										if (r.getValue().getFailCount() > 0) {
-											reportString.append("| " + r.getKey() + "         | "
-													+ Math.round(r.getValue().getDuration() / 60) + " min | "
-													+ r.getValue().getTotalCount() + " |" + r.getValue().getFailCount()
-													+ " | " + r.getValue().getSkipCount() + "|   |\n");
-											for (TestSuite suite : r.getValue().getSuites()) {
-												List<TestCase> casesToDisplay = Lists.newArrayList(
-														Iterables.filter(suite.getCases(), new Predicate<TestCase>() {
-
-															@Override
-															public boolean apply(TestCase input) {
-																return "FAILED".equals(input.getStatus())
-																		&& input.getAge() < 3;
-															}
-														}));
-												if (casesToDisplay.size() > 0) {
-													hasRecentRegressions = true;
-													for (TestCase tCase : casesToDisplay) {
-														String className = tCase.getClassName();
-														if (className.lastIndexOf(".") != -1) {
-															className = className
-																	.substring(className.lastIndexOf(".") + 1);
-														}
-														if (tCase.getAge() <= 2) {
-															reportString.append("|**" + className + "."
-																	+ tCase.getName() + "**|"
-																	+ Math.round(tCase.getDuration()) + " sec | " + " "
-																	+ "|" + " " + "|" + " " + "|**" + tCase.getAge()
-																	+ "**|\n");
-														} else {
-															reportString.append("|" + className + "."
-																	+ tCase.getName() + "| "
-																	+ Math.round(tCase.getDuration()) + " sec|" + " "
-																	+ "|" + " " + "|" + " " + "|" + tCase.getAge()
-																	+ "|\n");
-														}
-
-													}
-
-												}
-
-											}
-										}
-									}
+									hasRecentRegressions = hasRecentRegressions
+											|| generatePerTestCaseReport(reports, reportString);
 									if (reports.entrySet().size() > 0) {
 										testsResults = reportString.toString();
 									}
@@ -213,6 +161,165 @@ public class JenkinsLogger {
 		return posts;
 	}
 
+	private Predicate<TestCase> isRecentRegression = new Predicate<TestCase>() {
+
+		@Override
+		public boolean apply(TestCase input) {
+			return "FAILED".equals(input.getStatus()) && input.getAge() < 40;
+		}
+	};
+
+	private boolean generatePerTestCaseReport(Map<String, TestReport> reports, StringBuffer out) {
+		boolean hasRecentRegressions = false;
+		Multimap<String, TestReport> testNameToReports = HashMultimap.create();
+		for (Map.Entry<String, TestReport> r : reports.entrySet()) {
+			if (r.getValue().getFailCount() > 0) {
+				for (TestSuite suite : r.getValue().getSuites()) {
+
+					List<TestCase> casesToDisplay = Lists
+							.newArrayList(Iterables.filter(suite.getCases(), isRecentRegression));
+					if (casesToDisplay.size() > 0) {
+						hasRecentRegressions = true;
+						for (TestCase tCase : casesToDisplay) {
+							String testName = getTestName(tCase);
+							testNameToReports.put(testName, r.getValue());
+						}
+
+					}
+
+				}
+			}
+		}
+
+		out.append("| Test");
+		int platformIndex = 1;
+		Collection<TestReport> reportsWithFailures = Sets.newLinkedHashSet(testNameToReports.values());
+		for (Map.Entry<String, TestReport> r : reports.entrySet()) {
+			if (reportsWithFailures.contains(r.getValue())) {
+				String name = r.getKey().substring(r.getKey().indexOf("./") + 2);
+				if (name.indexOf("PLATFORM=") != -1) {
+					name = name.substring(name.indexOf("PLATFORM=") + 9);
+					if (name.indexOf(",") != -1) {
+						name = name.substring(0, name.indexOf(","));
+					}
+
+				}
+				out.append('|');
+				out.append("[" + name + "](" + r.getKey() + ")");
+				platformIndex++;
+			}
+		}
+		out.append("|\n");
+		out.append("|----------");
+		for (int i = 0; i < reports.entrySet().size(); i++) {
+			out.append("|:--------");
+		}
+		out.append("|\n");
+		for (String testName : testNameToReports.keySet()) {
+			Collection<TestReport> configWhereItFails = testNameToReports.get(testName);
+			boolean hasRecentFailure = false;
+			for (TestReport r : reportsWithFailures) {
+				if (configWhereItFails.contains(r)) {
+					TestCase tCase = findTestCaseFromName(testName, r);
+					if (tCase != null) {
+						if (tCase.getAge() <= 10) {
+							hasRecentFailure = true;
+						}
+
+					}
+				}
+			}
+			if (hasRecentFailure) {
+				out.append("|**" + testName + "**");
+			} else {
+				out.append("|" + testName);
+			}
+
+			for (TestReport r : reportsWithFailures) {
+				if (configWhereItFails.contains(r)) {
+					out.append("|");
+					TestCase tCase = findTestCaseFromName(testName, r);
+					if (tCase != null) {
+						if (hasRecentFailure) {
+							out.append("**");
+						}
+						out.append(tCase.getAge());
+						if (hasRecentFailure) {
+							out.append("**");
+						}
+					} else {
+						out.append('X');
+					}
+				} else {
+					out.append("| ");
+				}
+			}
+			out.append("|\n");
+
+		}
+		return hasRecentRegressions;
+	}
+
+	private TestCase findTestCaseFromName(String testName, TestReport r) {
+		TestCase found = null;
+		Iterator<TestSuite> it = r.getSuites().iterator();
+		while (found == null && it.hasNext()) {
+			TestSuite s = it.next();
+			Iterator<TestCase> itC = s.getCases().iterator();
+			while (found == null && itC.hasNext()) {
+				TestCase c = itC.next();
+				if (testName.equals(getTestName(c))) {
+					found = c;
+				}
+
+			}
+		}
+		return found;
+	}
+
+	private String getTestName(TestCase tCase) {
+		String className = tCase.getClassName();
+		if (className.lastIndexOf(".") != -1) {
+			className = className.substring(className.lastIndexOf(".") + 1);
+		}
+		String testName = className + "." + tCase.getName();
+		return testName;
+	}
+
+	private boolean generatePerConfigurationReport(Map<String, TestReport> reports, StringBuffer out) {
+		boolean hasRecentRegressions = false;
+		out.append("| Configuration        | Duration  | All  | Failed | Skipped | Age |\n");
+		out.append("| -------------------- |:---------:| ----:|-------:|--------:|-----|\n");
+		for (Map.Entry<String, TestReport> r : reports.entrySet()) {
+			if (r.getValue().getFailCount() > 0) {
+				out.append("| " + r.getKey() + "         | " + Math.round(r.getValue().getDuration() / 60) + " min | "
+						+ r.getValue().getTotalCount() + " |" + r.getValue().getFailCount() + " | "
+						+ r.getValue().getSkipCount() + "|   |\n");
+				for (TestSuite suite : r.getValue().getSuites()) {
+					List<TestCase> casesToDisplay = Lists
+							.newArrayList(Iterables.filter(suite.getCases(), isRecentRegression));
+					if (casesToDisplay.size() > 0) {
+						hasRecentRegressions = true;
+						for (TestCase tCase : casesToDisplay) {
+							String testName = getTestName(tCase);
+							if (tCase.getAge() <= 2) {
+								out.append("|**" + testName + "**|" + Math.round(tCase.getDuration()) + " sec | " + " "
+										+ "|" + " " + "|" + " " + "|**" + tCase.getAge() + "**|\n");
+							} else {
+								out.append("|" + testName + "| " + Math.round(tCase.getDuration()) + " sec|" + " " + "|"
+										+ " " + "|" + " " + "|" + tCase.getAge() + "|\n");
+							}
+
+						}
+
+					}
+
+				}
+			}
+		}
+		return hasRecentRegressions;
+	}
+
 	public static Map<String, TestReport> allNonEmptyReports(TestReport rep, String rootURL) {
 		Map<String, TestReport> result = Maps.newLinkedHashMap();
 		if (rep.getChildReports().size() > 0) {
@@ -221,8 +328,7 @@ public class JenkinsLogger {
 				if (childTestResults.getSkipCount() > 0 || childTestResults.getPassCount() > 0) {
 					String name = childReport.getChild().getUrl()
 							.substring(childReport.getChild().getUrl().indexOf("./") + 2);
-					String url = "[" + name + "](" + childReport.getChild().getUrl() + ")";
-					result.put(url, childTestResults);
+					result.put(childReport.getChild().getUrl(), childTestResults);
 				}
 			}
 		} else if (rep.getTotalCount() > 0) {
