4a565146a0f509f1962b95c1c876c683211f7df3
cbrun <cedric.brun@obeo.fr>
2017-03-20 12:39:47 +0100

    Split posts which are two bigs in several

--- stuart/src/main/java/fr/obeo/tools/stuart/mattermost/MattermostEmitter.java
+++ stuart/src/main/java/fr/obeo/tools/stuart/mattermost/MattermostEmitter.java
@@ -1,17 +1,17 @@
 package fr.obeo.tools.stuart.mattermost;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
+import java.util.List;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSession;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.Call;
-import com.squareup.okhttp.CertificatePinner;
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.HttpUrl;
 import com.squareup.okhttp.OkHttpClient;
@@ -43,29 +43,45 @@ public class MattermostEmitter {
 			public boolean verify(String hostname, SSLSession session) {
 				return true;
 			}
-		});	
+		});
 	}
 
-	public void accept(MattermostPost post) {
-		// gson.toJson(post)
-		RequestBody body = new FormEncodingBuilder().add("payload", gson.toJson(post)).build();
+	public void accept(MattermostPost inPost) {
+		List<MattermostPost> toSend = Lists.newArrayList();
+		/*
+		 * the actual limit is 4000 in mattermost.
+		 */
+		if (inPost.getText().length() > 3900) {
 
-		HttpUrl url = new HttpUrl.Builder().scheme(scheme).host(host).addPathSegment("hooks").addPathSegment(channel)
-				.build();
-		System.out.println(url);
-		Request request = new Request.Builder().url(url).post(body).build();
-		Response response;
-		try {
-			Call call = client.newCall(request);
-			response = call.execute();
-			if (!response.isSuccessful()) {
-				throw new IOException("Unexpected code " + response + " canceled ?:" + call.isCanceled());
+			for (String subPost : Splitter.fixedLength(3900).split(inPost.getText())) {
+				MattermostPost splitPost = new MattermostPost(subPost, inPost.getUsername(), inPost.getIconUrl());
+				toSend.add(splitPost);
 			}
-			System.out.println(response.message());
-			response.body().close();
-		} catch (IOException e) {
-			System.out.println("Message was length:" + post.getText().length() + "\n" + post);
-			throw new RuntimeException(e);
+		} else {
+			toSend.add(inPost);
+		}
+
+		for (MattermostPost post : toSend) {
+			RequestBody body = new FormEncodingBuilder().add("payload", gson.toJson(post)).build();
+
+			HttpUrl url = new HttpUrl.Builder().scheme(scheme).host(host).addPathSegment("hooks")
+					.addPathSegment(channel).build();
+			System.out.println(url);
+			Request request = new Request.Builder().url(url).post(body).build();
+			Response response;
+			try {
+				Call call = client.newCall(request);
+				response = call.execute();
+				if (!response.isSuccessful()) {
+					throw new IOException("Unexpected code " + response + " canceled ?:" + call.isCanceled());
+				}
+				System.out.println(response.message());
+				response.body().close();
+			} catch (IOException e) {
+				System.out.println("Message was length:" + post.getText().length() + "\n" + post);
+				throw new RuntimeException(e);
+			}
+
 		}
 
 	}
